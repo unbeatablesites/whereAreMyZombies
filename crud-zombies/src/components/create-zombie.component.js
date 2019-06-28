0a2c7d0958e2c307a928e26127873febefe53870
Fuzoka <unbeatablesites09@gmail.com>
2019-06-28 17:41:09 -0400

    Adding Create

--- crud-zombies/src/components/create-zombie.component.js
+++ crud-zombies/src/components/create-zombie.component.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 
-export default class ZombieList extends Component {
+export default class CreateZombie extends Component {
   render() {
     return (
       <div>
