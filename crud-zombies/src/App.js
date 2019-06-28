dafb01bf8779b9c23a7c85fd09a6d0d56146040f
Fuzoka <unbeatablesites09@gmail.com>
2019-06-28 17:04:09 -0400

    Bootstrap Test

--- crud-zombies/src/App.js
+++ crud-zombies/src/App.js
@@ -1,26 +1,13 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React, { Component } from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+class App extends Component {
+  render() {
+    return (
+      <div className="container">
+        <h2>Where are my zombies</h2>
+      </div>
+    );
+  }
 }
 
 export default App;
