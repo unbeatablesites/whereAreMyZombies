d0bbb96da235f4c5aafc2ef7a788d913dee738b6
Fuzoka <unbeatablesites09@gmail.com>
2019-06-28 17:31:38 -0400

    Test Components

---
+++ crud-zombies/src/components/edit-zombie.component.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+
+export default class EditZombie extends Component {
+  render() {
+    return (
+      <div>
+        <p>Edit the zombies!</p>
+      </div>
+    );
+  }
+}
