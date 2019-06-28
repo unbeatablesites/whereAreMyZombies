c45c5e914d78de8c45f70c62713b07c1aa53930c
Fuzoka <unbeatablesites09@gmail.com>
2019-06-28 17:39:39 -0400

    Test the routes

--- crud-zombies/src/App.js
+++ crud-zombies/src/App.js
@@ -1,6 +1,9 @@
 import React, { Component } from "react";
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
+import CreateZombie from "./components/create-zombie.component";
+import EditZombie from "./components/edit-zombie.component";
+import ZombieList from "./components/zombie-list.component";
 
 class App extends Component {
   render() {
@@ -8,10 +11,10 @@ class App extends Component {
       <Router>
         <div className="container">
           <h2>Where are my zombies</h2>
+          <Route path="/" exact component={ZombieList} />
+          <Route path="/edit/:id" component={EditZombie} />
+          <Route path="/create" component={CreateZombie} />
         </div>
-        <Route path="/" exact component={ZombieList} />
-        <Route path="/edit/:id" component={EditZombie} />
-        <Route path="/create" component={CreateZombie} />
       </Router>
     );
   }
