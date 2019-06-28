350984e38420ad5031705ff27ff00b7285667f8e
Fuzoka <unbeatablesites09@gmail.com>
2019-06-28 17:31:51 -0400

    Test Components

---
+++ crud-zombies/src/components/zombie-list.component.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+
+export default class ZombieList extends Component {
+  render() {
+    return (
+      <div>
+        <p>Here are all the zombies!</p>
+      </div>
+    );
+  }
+}
