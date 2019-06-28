da10d15a448808fc9300469f36268c542b1cd2d9
Fuzoka <unbeatablesites09@gmail.com>
2019-06-28 18:53:51 -0400

    Adding Event methods

--- crud-zombies/src/components/create-zombie.component.js
+++ crud-zombies/src/components/create-zombie.component.js
@@ -1,6 +1,45 @@
 import React, { Component } from "react";
 
 export default class CreateZombie extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      zombie_description: "",
+      zombie_responsible: "",
+      zombie_building: "",
+      zombie_alive: false
+    };
+  }
+
+  onChangeZombieDescription(e) {
+    this.setState({
+      zombie_description: e.target.value
+    });
+  }
+
+  onChangeZombieResponsible(e) {
+    this.setState({
+      zombie_responsible: e.target.value
+    });
+  }
+
+  onChangeZombieBuilding(e) {
+    this.setState({
+      zombie_building: e.target.value
+    });
+  }
+
+  onSubmit(e) {
+    e.preventDefault();
+    this.setState({
+      zombie_description: "",
+      zombie_responsible: "",
+      zombie_building: "",
+      zombie_alive: false
+    });
+  }
+
   render() {
     return (
       <div>
