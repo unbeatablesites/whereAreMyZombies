5dde20242780840d87213c1176742ee4cef01cd3
Fuzoka <unbeatablesites09@gmail.com>
2019-06-28 17:18:05 -0400

    Adding Router and routes

--- crud-zombies/src/App.js
+++ crud-zombies/src/App.js
@@ -1,11 +1,17 @@
 import React, { Component } from "react";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
+
 class App extends Component {
   render() {
     return (
-      <div className="container">
-        <h2>Where are my zombies</h2>
-      </div>
+      <Router>
+        <div className="container">
+          <h2>Where are my zombies</h2>
+        </div>
+        <Route path="/" exact component={ZombieList} />
+        <Route path="/edit/:id" component={EditZombie} />
+      </Router>
     );
   }
 }
