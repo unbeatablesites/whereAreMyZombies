5d5ccbe2b0048b208a7e903bf0306ba36d3fa42e
Fuzoka <unbeatablesites09@gmail.com>
2019-06-28 17:31:27 -0400

    Test Components

---
+++ crud-zombies/src/components/create-zombie.component.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+
+export default class ZombieList extends Component {
+  render() {
+    return (
+      <div>
+        <p>Here we can create zombies!</p>
+      </div>
+    );
+  }
+}
