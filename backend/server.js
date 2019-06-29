6e7d28b551e56a251a38ec46391816f2b4ad3752
Fuzoka <unbeatablesites09@gmail.com>
2019-06-28 20:47:13 -0400

    Init MongoDB

--- backend/server.js
+++ backend/server.js
@@ -7,5 +7,5 @@ const PORT = 4000;
 app.use(cors());
 app.use(bodyParser.json());
 app.listen(PORT, function() {
-  console.log("server is running on Port:" + PORT);
+  console.log("Server is running on Port:" + PORT);
 });
